77632bb1d3c5fb6f97e35c67277bdc50cacea884
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-03-24 16:22:56 +0100

    update_2

---
+++ main.js
@@ -0,0 +1,11 @@
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+vue app = new Vue (
+  {
+    el:'#rootVue',
+    text: 'Hello World'
+  }
+);
